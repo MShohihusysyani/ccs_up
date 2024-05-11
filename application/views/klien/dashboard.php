e7a47df2b2f22f75d1fb66cb636ef646cc518118
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-05-11 12:49:39 +0700

    CCS

--- application/views/klien/dashboard.php
+++ application/views/klien/dashboard.php
@@ -1,112 +1,94 @@
 
+<body class="theme-red">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Please wait...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
 
-<section class="content">
+    <section class="content">
     <div class="login" data-login="<?= $this->session->flashdata('pesan') ?>">
         <?php if ($this->session->flashdata('pesan')) { ?>
 
         <?php } ?>
-    <!-- <div class="container-fluid">
+
+        <div class="container-fluid">
             <div class="block-header">
                 <h2>DASHBOARD</h2>
-            </div> -->
+            </div>
 
-    <!-- Widgets -->
-    <!-- <div class="row clearfix">
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-pink hover-expand-effect">
-                        <div class="icon">
-                            <i class="material-icons">playlist_add_check</i>
-                        </div>
-                        <div class="content">
-                            <div class="text">NEW TASKS</div>
-                            <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-cyan hover-expand-effect">
-                        <div class="icon">
-                            <i class="material-icons">help</i>
-                        </div>
-                        <div class="content">
-                            <div class="text">NEW TICKETS</div>
-                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-light-green hover-expand-effect">
-                        <div class="icon">
-                            <i class="material-icons">forum</i>
-                        </div>
-                        <div class="content">
-                            <div class="text">NEW COMMENTS</div>
-                            <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-orange hover-expand-effect">
-                        <div class="icon">
-                            <i class="material-icons">person_add</i>
-                        </div>
-                        <div class="content">
-                            <div class="text">NEW VISITORS</div>
-                            <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-     <!-- #END# Widgets -->
-     <!-- CPU Usage -->
-    <div class="row clearfix">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="card">
-                <div class="header">
-                    <div class="row clearfix">
-                        <div class="col-xs-12 col-sm-6">
-
-                            <h4>SELAMAT DATANG DI SISTEM CUSTOMER CARE SYSTEM</h4>
-
-
-                        </div>
-                        <div class="col-xs-12 col-sm-6 align-right">
-                            <div class="switch panel-switch-btn">
-                                <!-- <span class="m-r-10 font-12">REAL TIME</span>
-                                        <label>OFF<input type="checkbox" id="realtime" checked><span class="lever switch-col-cyan"></span>ON</label> -->
+            <div class="row clearfix">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <div class="row clearfix">
+                                <div class="col-xs-12 col-sm-6">
+                                    <h2>SELAMAT DATANG DI CUSTOMER CARE SYSTEM</h2>
+                                </div>
+                                
                             </div>
+                            <ul class="header-dropdown m-r--5">
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        <i class="material-icons">more_vert</i>
+                                    </a>
+                                    
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="body">
+
+                            <div class="row clearfix">
+                                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                    <div class="info-box bg-blue hover-expand-effect">
+                                        <div class="icon">
+                                            <a href="<?php echo base_url('klien/pengajuan') ?>">
+                                                <i class="material-icons">assignment_turned_in</i>
+                                            </a>
+                                        </div>
+                                            <div class="content">
+                                        <div class="text">NEW TIKET</div>
+                                    </div>
+                                    </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                <div class="info-box bg-cyan hover-expand-effect">
+                                    <div class="icon">
+                                        <a href="<?php echo base_url('klien/bank_knowlage') ?>">
+                                            <i class="material-icons">account_balance</i>
+                                        </a>
+                                    </div>
+                                    <div class="content">
+                                        <div class="text">BANK KNOWLAGE</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END FINISH -->
+                            
                         </div>
                     </div>
-
-                    <h5><b>DATA PELAPORAN</b></h5>
-                    <br>
-                    
-                    <ul class="header-dropdown m-r--5">
-                        <li class="dropdown">
-                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <i class="material-icons">more_vert</i>
-                            </a>
-                            <ul class="dropdown-menu pull-right">
-                                <li><a href="javascript:void(0);">Action</a></li>
-                                <li><a href="javascript:void(0);">Another action</a></li>
-                                <li><a href="javascript:void(0);">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <div class="body">
-
-
-
-                    <!-- <div id="real_time_chart" class="dashboard-flot-chart"></div> -->
                 </div>
             </div>
+
+            <div class="row clearfix">
+            </div>
+
         </div>
-    </div>
-    </div>
-
-    </div>
-
-
-    </div>
 </section>
+
