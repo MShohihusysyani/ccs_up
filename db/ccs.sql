8c99365f84a19c2baff92a91af03151470950299
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-05-11 12:58:23 +0700

    Update database

--- db/ccs.sql
+++ db/ccs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Bulan Mei 2024 pada 07.29
+-- Waktu pembuatan: 11 Bulan Mei 2024 pada 07.57
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 7.4.26
 
