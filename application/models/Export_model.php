4743f42e5e409fb58a5dbcb36394b6d132efbc04
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-05-27 10:47:14 +0700

    CCS

--- application/models/Export_model.php
+++ application/models/Export_model.php
@@ -1,10 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Export_model extends CI_Model {
-	public function getPelaporan($tgla, $tglb)
+	public function getPelaporan()
     {
         $query = "SELECT *
-                    FROM pelaporan WHERE waktu_pelapaporan BETWEEN '$tgla' AND '$tglb'";
+                    FROM pelaporan";
         return $this->db->query($query)->result_array();
     }
 }
