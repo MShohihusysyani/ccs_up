5b25274cfe8e846d44c47263898db7dd520d4a70
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-06-19 11:12:31 +0700

    CCS

--- application/views/superadmin/rekap_pelaporan.php
+++ application/views/superadmin/rekap_pelaporan.php
@@ -113,7 +113,7 @@
                 </div>
                 <div class="body">
                     <div class="table-responsive">
-                        <table class="table table-bordered table-striped table-hover js-exportable dataTable" id="example">
+                        <table class="table table-bordered table-striped table-hover dataTable js-basic-example" id="example">
                             <thead>
                                 <tr>
                                     <th>No</th>
