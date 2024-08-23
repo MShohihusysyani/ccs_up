0aea6622f840e3e27c475c4370fabca770f5a302
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-08-23 10:45:37 +0700

    CCS

--- application/views/implementator/close.php
+++ application/views/implementator/close.php
@@ -33,7 +33,7 @@
                                             <th>No Tiket</th>
                                             <th>Tanggal</th>
                                             <th>Nama Klien</th>
-                                            <th>Perihal</th>
+                                            <th>Judul</th>
                                             <th>Impact</th>
                                             <th>Attachment</th>
                                             <th>Category</th>
@@ -54,7 +54,7 @@
                                             <th>No Tiket</th>
                                             <th>Tanggal</th>
                                             <th>Nama Klien</th>
-                                            <th>Perihal</th>
+                                            <th>Judul</th>
                                             <th>Impact</th>
                                             <th>Attachment</th>
                                             <th>Category</th>
@@ -79,7 +79,7 @@
                                                 <td><?= $dp['no_tiket']; ?></td>
                                                 <td><?= tanggal_indo($dp['waktu_pelaporan']) ?></td>
                                                 <td><?= $dp['nama']; ?></td>
-                                                <td><?= $dp['perihal']; ?></td>
+                                                <td><?= $dp['judul']; ?></td>
                                                 <td><?= $dp['impact']; ?></td>
                                                 <td> <a href="<?= base_url('assets/files/' . $dp['file']); ?>"><?= $dp['file']; ?></a>
                                                 </td>
