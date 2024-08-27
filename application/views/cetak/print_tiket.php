f3811ba051922ea40ddd4dbb19a6383a8756bb62
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-08-27 12:10:40 +0700

    Perbaikan view print tiket

--- application/views/cetak/print_tiket.php
+++ application/views/cetak/print_tiket.php
@@ -4,6 +4,107 @@
 <head>
     <title>Ticket Detail</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style type="text/css">
+        @media print {
+            .container {
+                width: 100%;
+            }
+
+            .form-group {
+                display: flex;
+                margin-bottom: 10px;
+            }
+
+            .form-control-static {
+                font-size: 12px;
+                margin-left: 10px;
+            }
+
+            textarea.form-control {
+                width: 90%;
+                font-size: 12px;
+                margin-left: 10px;
+            }
+
+            @page {
+                size: A4;
+                margin: 20mm;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>Detail #<?= $ticket->no_tiket ?></h3>
+                <h4><?= $ticket->judul ?></h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2"><b>Added By:</b></div>
+            <div class="col-sm-10">
+                <p class="form-control-static"><?= $ticket->nama ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2"><b>BPR/Client:</b></div>
+            <div class="col-sm-10">
+                <p class="form-control-static"><?= $ticket->nama ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2"><b>Details:</b></div>
+            <div class="col-sm-10"><textarea class="form-control" rows="10"><?= $ticket->perihal ?></textarea></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2"><b>Category:</b></div>
+            <div class="col-sm-10">
+                <p class="form-control-static"><?= $ticket->kategori ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2"><b>Priority:</b></div>
+            <div class="col-sm-10">
+                <p class="form-control-static"><?= $ticket->priority ?></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2"><b>Status:</b></div>
+            <div class="col-sm-10">
+                <p class="form-control-static"><?= $ticket->status_ccs ?></p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-2"><b>Status:</b></div>
+            <div class="col-sm-10">
+                <?php
+                // Ambil semua handler
+                $handlers = array_filter([$ticket->handle_by, $ticket->handle_by2, $ticket->handle_by3]);
+                foreach ($handlers as $handler) : ?>
+                    <p class="form-control-static"><?= $handler ?></p>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
+        <!-- Add more rows for other fields -->
+    </div>
+
+    <script type="text/javascript">
+        window.print();
+    </script>
+</body>
+
+</html>
+
+<!-- <!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ticket Detail</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
@@ -97,7 +198,7 @@
             </div>
         </div>
         <div class="form-group">
-            <!-- <label class="col-sm-2 control-label"><b>Perihal:</b></label> -->
+            <label class="col-sm-2 control-label"><b>Perihal:</b></label>
             <div class="col-sm-10">
                 <textarea class="form-control" rows="10"><?= $ticket->perihal ?></textarea>
             </div>
@@ -139,4 +240,4 @@
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
-</html>
+</html> -->
