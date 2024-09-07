81d9e661f28b3b21a52490aab885d3a0b9ef89f3
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-09-07 11:43:55 +0700

    perubahan detail pelaporan handle 1

--- application/views/helpdesk/detail_finish.php
+++ application/views/helpdesk/detail_finish.php
@@ -71,6 +71,35 @@
                                 </div>
                             </div>
 
+                            <!-- <label for="nama">File</label>
+                            <div class="form-group">
+                                <div class="form-line">
+                                <?php
+                                $file_finish = $dp['file']; // File yang dilampirkan
+                                if (!empty($file_finish)) {
+                                    $file_path = base_url('assets/files/') . $file_finish;
+                                    $file_name = basename($file_finish);  // Mengambil nama file
+                                    $file_ext = pathinfo($file_finish, PATHINFO_EXTENSION);
+
+                                    // Tampilkan nama file
+                                    echo '<p>' . $file_name . '</p>';
+
+                                    // Periksa apakah file adalah gambar
+                                    if (in_array($file_ext, ['jpg', 'jpeg', 'png', 'gif'])) {
+                                        // Jika file gambar, tampilkan dengan tag <img>
+                                        echo '<img src="' . $file_path . '" width="500" height="500" class="img-thumbnail">';
+                                    } else {
+                                        // Jika file bukan gambar, tampilkan tautan untuk mengunduh atau melihat file
+                                        echo '<a href="' . $file_path . '" target="_blank" class="btn btn-primary">Download file</a>';
+                                    }
+                                } else {
+                                    // Jika tidak ada file yang dilampirkan
+                                    echo '<span class= "label label-info" style="font-size:12px;">Tidak ada file yang dilampirkan.</span/';
+                                }
+                                ?>
+                                </div>
+                            </div> -->
+
                             <label for="perihal">Catatan Finish</label>
                             <textarea id="editor" class="form-control" name="catatan_finish" id="catatan_finish" readonly>
                                 <?= $dp['catatan_finish']; ?>
@@ -79,7 +108,29 @@
                             <label for="nama">File</label>
                             <div class="form-group">
                                 <div class="form-line">
-                                    <img src="<?= base_url('assets/files/') . $dp['file']; ?>" width="500" height="500" class="img-thumbnail">
+                                    <?php
+                                    $file_finish = $dp['file_finish']; // File yang dilampirkan
+                                    if (!empty($file_finish)) {
+                                        $file_path = base_url('assets/filefinish/') . $file_finish;
+                                        $file_name = basename($file_finish);  // Mengambil nama file
+                                        $file_ext = pathinfo($file_finish, PATHINFO_EXTENSION);
+
+                                        // Tampilkan nama file
+                                        echo '<p>' . $file_name . '</p>';
+
+                                        // Periksa apakah file adalah gambar
+                                        if (in_array($file_ext, ['jpg', 'jpeg', 'png', 'gif'])) {
+                                            // Jika file gambar, tampilkan dengan tag <img>
+                                            echo '<img src="' . $file_path . '" width="500" height="500" class="img-thumbnail">';
+                                        } else {
+                                            // Jika file bukan gambar, tampilkan tautan untuk mengunduh atau melihat file
+                                            echo '<a href="' . $file_path . '" target="_blank" class="btn btn-primary">Download file</a>';
+                                        }
+                                    } else {
+                                        // Jika tidak ada file yang dilampirkan
+                                        echo '<span class= "label label-info" style="font-size:12px;">Tidak ada file yang dilampirkan.</span/';
+                                    }
+                                    ?>
                                 </div>
                             </div>
 
