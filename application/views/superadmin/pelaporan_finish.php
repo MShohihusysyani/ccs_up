d3ed1b1c150a9d02a8d3d9892fd442eb04393fbf
Shohi <81415646+MShohihusysyani@users.noreply.github.com>
2024-10-03 16:07:10 +0700

    perubahan view menu finish superadmin mjd serverside datatable

--- application/views/superadmin/pelaporan_finish.php
+++ application/views/superadmin/pelaporan_finish.php
@@ -28,6 +28,9 @@
 
             </h2>
         </div>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css">
+        <script type="text/javascript" src="//code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
         <!-- Basic Examples -->
         <div class="login" data-login="<?= $this->session->flashdata('pesan') ?>">
             <?php if ($this->session->flashdata('pesan')) { ?>
@@ -46,7 +49,7 @@
                         </div>
                         <div class="body">
                             <div class="table-responsive">
-                                <table class="table table-bordered table-striped table-hover dataTable js-basic-example" id="example">
+                                <table class="table table-bordered table-striped table-hover" id="example">
                                     <!-- class="table table-bordered table-striped table-hover dataTable js-basic-example" -->
                                     <thead>
                                         <tr>
@@ -58,7 +61,7 @@
                                             <th>Category</th>
                                             <th>Tags</th>
                                             <th>Priority</th>
-                                            <th>Impact</th>
+                                            <!-- <th>Impact</th> -->
                                             <th>Max Day</th>
                                             <th>Status CCS</th>
                                             <th>Handle By</th>
@@ -78,7 +81,7 @@
                                             <th>Category</th>
                                             <th>Tags</th>
                                             <th>Priority</th>
-                                            <th>Impact</th>
+                                            <!-- <th>Impact</th> -->
                                             <th>Max Day</th>
                                             <th>Status CCS</th>
                                             <th>Handle By</th>
@@ -89,129 +92,6 @@
                                     </tfoot>
                                     <tbody>
 
-                                        <?php
-                                        $no = 1;
-                                        foreach ($datapelaporan as $dp) : ?>
-                                            <tr>
-                                                <td><?= $no++ ?></td>
-                                                <td><?= $dp['no_tiket']; ?></td>
-                                                <td><?= tanggal_indo($dp['waktu_pelaporan']) ?></td>
-                                                <td><?= $dp['nama']; ?></td>
-                                                <td><?= $dp['judul']; ?></td>
-                                                <td><?= $dp['kategori']; ?></td>
-                                                <td>
-                                                    <?php if (!empty($dp['tags'])): ?>
-                                                        <span class="label label-info">
-                                                            <?= $dp['tags']; ?>
-                                                        </span>
-                                                    <?php endif; ?>
-                                                </td>
-                                                <td>
-                                                    <?php if ($dp['priority'] == 'Low') : ?>
-                                                        <span class="label label-info">Low</span>
-
-                                                    <?php elseif ($dp['priority'] == 'Medium') : ?>
-                                                        <span class="label label-warning">Medium</span>
-
-                                                    <?php elseif ($dp['priority'] == 'High') : ?>
-                                                        <span class="label label-danger">High</span>
-
-                                                    <?php else : ?>
-                                                    <?php endif; ?>
-                                                </td>
-                                                <td><?= $dp['impact']; ?></td>
-                                                <td>
-                                                    <?php if ($dp['maxday'] == '90') : ?>
-                                                        <span class="label label-info">90</span>
-
-                                                    <?php elseif ($dp['maxday'] == '60') : ?>
-                                                        <span class="label label-warning">60</span>
-
-                                                    <?php elseif ($dp['maxday'] == '7') : ?>
-                                                        <span class="label label-danger">7</span>
-
-                                                    <?php else : ?>
-                                                    <?php endif; ?>
-                                                </td>
-                                                <td>
-                                                    <?php if ($dp['status_ccs'] == 'FINISHED') : ?>
-                                                        <span class="label label-success">FINISHED</span>
-
-                                                    <?php elseif ($dp['status_ccs'] == 'CLOSED') : ?>
-                                                        <span class="label label-warning">CLOSED</span>
-
-                                                    <?php elseif ($dp['status_ccs'] == 'HANDLED') : ?>
-                                                        <span class="label label-info">HANDLED</span>
-
-                                                    <?php elseif ($dp['status_ccs'] == 'ADDED') : ?>
-                                                        <span class="label label-primary">ADDED</span>
-
-                                                    <?php else : ?>
-                                                    <?php endif; ?>
-
-                                                </td>
-
-                                                <td>
-                                                    <?php
-                                                    // Contoh penanganan multiple handle by
-                                                    $handleByList = [
-                                                        $dp['handle_by'],
-                                                        $dp['handle_by2'],
-                                                        $dp['handle_by3']
-                                                    ];
-
-                                                    // Loop untuk setiap user yang menghandle tiket
-                                                    $first = true;
-                                                    foreach ($handleByList as $handleBy) :
-                                                        if (!empty($handleBy)) :
-                                                            // Tampilkan koma di antara nama-nama pengguna jika bukan pengguna pertama
-                                                            if (!$first) {
-                                                                echo ', ';
-                                                            }
-                                                            $first = false;
-                                                    ?>
-                                                            <div class="handle-by-item">
-                                                                <?= $handleBy; ?>
-                                                            </div>
-                                                    <?php
-                                                        endif;
-                                                    endforeach;
-                                                    ?>
-                                                </td>
-                                                <td>
-                                                    <!-- Tampilkan rating dalam bentuk bintang di kolom Rating -->
-                                                    <?php if ($dp['rating'] !== null) : ?>
-                                                        <div class="star-rating">
-                                                            <?php
-                                                            // Tampilkan bintang sesuai dengan rating yang diberikan
-                                                            $rating = $dp['rating'];
-                                                            for ($i = 1; $i <= 5; $i++) {
-                                                                if ($i <= $rating) {
-                                                                    echo '<span class="star selected">&#9733;</span>';
-                                                                }
-                                                            }
-                                                            ?>
-                                                        </div>
-                                                    <?php endif; ?>
-                                                </td>
-
-                                                <td><a class="btn btn-sm btn-info" href="<?= base_url() ?>superadmin/detail_finish/<?= $dp['id_pelaporan']; ?>"><i class="material-icons">visibility</i> <span class="icon-name"></span>
-                                                        Detail</a>
-                                                </td>
-
-                                                <!-- <td>
-                                                    <?= $dp['handle_by']; ?>
-                                                    <?php if (!empty($dp['handle_by2'])) : ?>
-                                                        , <?= $dp['handle_by2']; ?>
-                                                    <?php endif; ?>
-                                                    <?php if (!empty($dp['handle_by3'])) : ?>
-                                                        , <?= $dp['handle_by3']; ?>
-                                                    <?php endif; ?>
-                                                </td> -->
-
-                                                <!-- <td><?= tanggal_indo($dp['waktu_approve']); ?></td> -->
-                                            </tr>
-                                        <?php endforeach; ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -223,3 +103,21 @@
         </div>
         <!-- Button trigger modal -->
 </section>
+
+<script>
+    $('#example').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "<?php echo site_url('superadmin/get_data_finish') ?>",
+            "type": "POST"
+        },
+        "order": [
+            [2, 'desc']
+        ], // Urutkan berdasarkan kolom ke-3 (indeks 2) secara descending (dari yang terbaru)
+        "columnDefs": [{
+            "targets": [0],
+            "orderable": false,
+        }, ],
+    });
+</script>
